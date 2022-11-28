f0f90bf22889afe887045cbc9638a951c57c3d4d
Mahdi Masoumpouraskari <m.masoumpouraskari@gmail.com>
2022-11-28 15:35:24 +0330

    added views code for first drop

--- app/views.py
+++ app/views.py
@@ -2,32 +2,94 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
+import os
 
 # Flask modules
-from flask import render_template, request
 from jinja2 import TemplateNotFound
-
+from flask import render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
 # App modules
-from app import app
+from app import app, ALLOWED_EXTENSIONS
+from py_data_converter import *
+from py_data_converter.converter_csv import convert_csv_to_django_models, convert_csv_to_flask_models
+from py_data_converter.converter_openapi import convert_openapi_json_to_django_models, \
+    convert_openapi_json_to_flask_models, convert_openapi_yaml_to_django_models, convert_openapi_yaml_to_flask_models
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def get_type(filename):
+    return filename.rsplit('.', 1)[1].lower()
 
 
 # App main route + generic routing
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    data = {}
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            # front
+            output_desired = request.form.get("outputDropdown")
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # front
+            if output_desired == 'dbmodel':
+                return redirect(url_for('output', name=filename))
+            elif output_desired == 'charts':
+                # to be implemented
+                ...
+            else:
+                # to be implemented
+                ...
 
-    data['stuff'] = 'whatever'
+    else:
+        # front
+        return render_template(home_page)
 
-    return render_template('converter/index.html', **data)
+
+@app.route('/output/<name>', methods=['GET', 'POST'])
+def pages(name):
+    if request.method == 'GET':
+        try:
+
+            type = get_type(name)
+            if type == 'csv':
+                django_response = convert_csv_to_django_models(app.config['UPLOAD_FOLDER'], name)
+                flask_response = convert_csv_to_flask_models(app.config['UPLOAD_FOLDER'], name)
+            elif type == 'json':
+                django_response = convert_openapi_json_to_django_models(app.config['UPLOAD_FOLDER'], name)
+                flask_response = convert_openapi_json_to_flask_models(app.config['UPLOAD_FOLDER'], name)
+            elif type == 'yml':
+                django_response = convert_openapi_yaml_to_django_models(app.config['UPLOAD_FOLDER'], name)
+                flask_response = convert_openapi_yaml_to_flask_models(app.config['UPLOAD_FOLDER'], name)
+
+            data = {'django': django_response, 'flask': flask_response}
+            # front
+            # Serve the file (if exists) from app/templates/FILE.html
+            return render_template(output_template, data)
+
+        except TemplateNotFound:
+            return render_template('pages/page-404.html'), 404
+    elif request.method == 'POST':
+        data = request.get_json()
+        request_django = data['django']
+        request_flask = data['flask']
+        # to be implemented
 
 
-@app.route('/<path>')
-def pages(path):
-    try:
 
-        # Serve the file (if exists) from app/templates/FILE.html
-        return render_template('pages/' + path)
 
-    except TemplateNotFound:
-        return render_template('pages/page-404.html'), 404
-
+if __name__ == '__main__':
+    print(allowed_file("products-with-price.json"))
