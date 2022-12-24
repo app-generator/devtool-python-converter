d6ab334bc4c29f414645a4969b3b2bdade10e0ca
Mahdi Masoumpouraskari <m.masoumpouraskari@gmail.com>
2022-12-25 02:16:12 +0330

    pkl bug fixed

--- py_data_converter/converter_pandas.py
+++ py_data_converter/converter_pandas.py
@@ -2,17 +2,14 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-import io
 
 import pandas as pd
-import os
-
-from app import app
-from py_data_converter.converter_csv import convert_csv_to_django_models, convert_csv_to_flask_models, parse_csv
-
-
 def convert_pandas_to_csv(file):
-    df = pd.read_pickle(file)
+    try:
+        df = pd.read_pickle(file)
+    except ValueError:
+        import pickle5
+        df = pickle5.load(file)
     df = df.set_index(df.columns[1])
     file1 = df.to_csv()
     return file1
